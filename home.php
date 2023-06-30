dacfe3f4e9f0eba0b492c12c9d7699045df9d454
Helder Malunga <heldermalunga@gmail.com>
2023-06-30 09:00:43 +0200

    versao 1.0

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+	<header>
+		<h1>Pagina Inicial</h1>
+		<p>Seja bem vindo, nome@gamil.com <a href="index.php">Sair</a></p>
+		
+	</header>
+
+</body>
+</html>
